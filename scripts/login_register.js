d16c47be83ebf0f3b1609a97ae211c5e0a6d6274
Andreas Brem <andi.brem@web.de>
2025-10-25 10:50:45 +0200

    login index.html and register.html and login_register.css

--- scripts/login_register.js
+++ scripts/login_register.js
@@ -1,2 +1,22 @@
-const BASE_URL = "https://remotestorage-8c4dc-default-rtdb.europe-west1.firebasedatabase.app";
+const BASE_URL = "https://join-kanban-app-14634-default-rtdb.europe-west1.firebasedatabase.app/user/1";
+let firebase = [];
 
+function addUser(){
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+
+}
+
+getGuestUserData();
+
+async function getGuestUserData(){
+    try {
+        let res = await fetch(BASE_URL + ".json");
+        let resToJson = await res.json();
+        firebase.push(resToJson);
+        console.log(firebase);  
+        return firebase
+    } catch (error) {
+        
+    }
+}
