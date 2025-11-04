21133b4b9c55ff5f7ca04fde20c94528f3360e03
Andreas Brem <andi.brem@web.de>
2025-11-04 16:04:11 +0100

    board task design completed

--- scripts/board.js
+++ scripts/board.js
@@ -15,6 +15,7 @@ async function renderTasks(activeUserId = 0) {
 
 function dragstartHandler(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
+    
 }
 function dragoverHandler(ev) {
     ev.preventDefault();
