7d7f4f433de2b7eaf14c39351ed61a42129baff8
Emin Fatih Halici <halicieminfatih@gmail.com>
2025-11-06 12:32:59 +0100

    feat: adding overlay function and overlay

--- scripts/board.js
+++ scripts/board.js
@@ -2,6 +2,28 @@ const BASE_URL = "https://join-kanban-app-14634-default-rtdb.europe-west1.fireba
 let urlParams = new URLSearchParams(window.location.search);
 let activeUserId = urlParams.get('activeUserId');
 
+let contactCircleColor = [
+    '#FF7A00',
+    '#FF5EB3',
+    '#6E52FF',
+    '#9327FF',
+    '#00BEE8',
+    '#1FD7C1',
+    '#FF745E',
+    '#FFA35E',
+    '#FC71FF',
+    '#FFC701',
+    '#0038FF',
+    '#C3FF2B',
+    '#FFE62B',
+    '#FF4646',
+    '#FFBB2B',
+]
+
+function init() {
+    renderOverlay();
+}
+
 async function renderTasks(activeUserId = 0) {
     try {
         let res = await fetch(BASE_URL + "/" + activeUserId + "/tasks" + ".json");
@@ -90,4 +112,88 @@ function renderTaskCard(task) {
             </div>
         </div>
     `;
+}
+function getAddTaskTemplate() {
+    return `
+       <section class="add-task-section">
+            <h1>Add Task</h1>
+
+            <form id="task-form" class="task-form">
+                <div class="form-left">
+                    <label for="title">Title*</label>
+                    <input id="title" type="text" placeholder="Enter a title">
+
+                    <label for="description">Description</label>
+                    <textarea id="description" placeholder="Enter a Description"></textarea>
+
+                    <label for="due-date">Due date*</label>
+                    <input id="due-date" type="date" required>
+
+                </div>
+
+                <div class="divider"></div>
+
+                <div class="form-right">
+                    <label>Priority</label>
+                    <div class="priority-buttons">
+                        <button type="button" class="priority-btn urgent">
+                            Urgent
+                            <img src="/assets/icons/prio_urgent_icon.svg" alt="urgent icon">
+                        </button>
+                        <button type="button" class="priority-btn medium active">
+                            Medium
+                            <img src="/assets/icons/prio_medium_icon.svg" alt="medium icon">
+                        </button>
+                        <button type="button" class="priority-btn low">
+                            Low
+                            <img src="/assets/icons/prio_low_icon.svg" alt="low icon">
+                        </button>
+                    </div>
+
+                    <label for="assigned">Assigned to</label>
+                    <div class="custom-select-container">
+                        <div id="assigned-display" class="select-display" onclick="toggleContactDropdown()">
+                            Select contacts to assign
+                        </div>
+
+                        <div id="assigned-dropdown" class="select-dropdown" style="display: none;">
+                        </div>
+                    </div>
+
+                    <label for="category">Category*</label>
+                    <select id="category" required>
+                        <option value="" disabled selected>Select task category</option>
+                        <option value="technical">Technical Task</option>
+                        <option value="user-story">User Story</option>
+                    </select>
+
+                    <label for="subtask">Subtasks</label>
+                    <input type="text" id="subtask" placeholder="Add new subtask">
+                </div>
+            </form>
+
+            <div class="form-footer">
+
+                <p class="form-hint">
+                    <span class="required-marker">*</span>This field is required
+                </p>
+
+
+                <div class="form-actions">
+                    <button id="clear-btn" type="button" class="clear">Clear ✖</button>
+                    <button id="create-btn" type="button" class="create">Create Task ✔</button>
+                </div>
+
+
+            </div>
+
+
+        </section>
+    `;
+}
+
+function renderOverlay() {
+
+    let overlay = document.getElementById("add-task-overlay");
+    overlay.innerHTML = getAddTaskTemplate();
 }
